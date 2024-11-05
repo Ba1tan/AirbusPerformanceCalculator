1c65ec3eb0e7c2686ff5791f32d8adb10e9cd120
Ba1tan <231620@astanait.edu.kz>
2024-11-05 10:45:22 +0500

    Calculation result page. Calculation Fragment completed

--- app/src/main/java/com/example/airbusperformancecalculator/CalcFragment.kt
+++ app/src/main/java/com/example/airbusperformancecalculator/CalcFragment.kt
@@ -1,59 +1,93 @@
 package com.example.airbusperformancecalculator
 
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.AutoCompleteTextView
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.google.android.material.textfield.TextInputLayout
+import com.google.gson.Gson
+private val calculation = Calculation()
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [CalcFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class CalcFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var userFlaps : EditText
+    private lateinit var userGW : EditText
+    private lateinit var userQNH : EditText
+    private lateinit var userCG : EditText
+    private lateinit var userTemperature : EditText
+    private lateinit var userAntiIce : EditText
+    private lateinit var userAirCond : EditText
+    private lateinit var userRunawayCond : EditText
+    private lateinit var userElevation : EditText
+    private lateinit var resV2 : TextView
+    private lateinit var resV1 : TextView
+    private lateinit var resVR : TextView
+    private lateinit var resTrim : TextView
+    private lateinit var resFLEX : TextView
+    private lateinit var aircraftType : TextView
+    private lateinit var aircraft : Aircraft
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_calc, container, false)
-    }
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?): View
+    {
+        val view : View = inflater.inflate(R.layout.fragment_calc, container, false)
+        userFlaps = view.findViewById(R.id.editTextFlaps)
+        userGW = view.findViewById(R.id.editTextGW)
+        userQNH = view.findViewById(R.id.editTextQNH)
+        userCG = view.findViewById(R.id.editTextCG)
+        userTemperature = view.findViewById(R.id.editTextTemperature)
+        userAntiIce = view.findViewById(R.id.editTextAntiIce)
+        userRunawayCond = view.findViewById(R.id.editTextRunawayCond)
+        userElevation = view.findViewById(R.id.editTextRunawayElev)
+        userAirCond = view.findViewById(R.id.editTextAirCond)
+        resV2 = view.findViewById(R.id.textV2)
+        resV1 = view.findViewById(R.id.textV1)
+        resVR = view.findViewById(R.id.textVR)
+        resFLEX = view.findViewById(R.id.textFLEX)
+        resTrim = view.findViewById(R.id.textTHS)
+        aircraftType = view.findViewById(R.id.textView1)
+        val textInputLayout : TextInputLayout = view.findViewById(R.id.inputLayout)
+        val autoCompleteTextView : AutoCompleteTextView = view.findViewById(R.id.inputTV)
+        val calculateButton = view.findViewById<Button>(R.id.calculateButton)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CalcFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CalcFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
+        autoCompleteTextView.onItemClickListener= AdapterView.OnItemClickListener{
+                parent, view, position, id -> val selectedItem = parent.getItemAtPosition(position) as String
+            aircraftType.text = selectedItem
+            Toast.makeText(requireContext(), "Selected: $selectedItem", Toast.LENGTH_SHORT).show()
+            var aFactory = AirbusFactory()
+            aircraft = aFactory.create(requireContext(), selectedItem)!!
+        }
+        calculateButton.setOnClickListener(){
+            val inputData = UserInput(
+                userFlaps.text.toString(),
+                userGW.text.toString().toInt(),
+                userCG.text.toString().toDouble(),
+                userQNH.text.toString().toInt(),
+                userTemperature.text.toString().toInt(),
+                userAntiIce.text.toString(),
+                userAirCond.text.toString(),
+                userRunawayCond.text.toString(),
+                userElevation.text.toString().toInt()
+            )
+            val flightData = FlightData(aircraft, inputData).createDataMap()
+            val results = CalculationResults(calculation).calculateAll(flightData)
+
+            resV1.text = results["V1"].toString()
+            resVR.text = results["VR"].toString()
+            resV2.text = results["V2"].toString()
+            resTrim.text = results["Trim"].toString()
+            resFLEX.text = results["FlexTemp"].toString()
+        }
+        return view
     }
 }
